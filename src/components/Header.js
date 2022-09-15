2b8d452205369f2575e8b5a78149daae8dbd6ddf
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-09-15 22:38:31 +0200

    header

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
+import themeSwitchIcon from "../assets/header/theme-switch.png";
+function Header() {
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} className="punkLogo" alt="" />
+      </div>
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} alt="" />
+        </div>
+        <input
+          className="searchInput"
+          placeholder="Collection, item or user..."
+        />
+      </div>
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+          <img src={themeSwitchIcon} alt="" />
+        </div>
+      </div>
+      <div className="loginButton">Get In</div>
+    </div>
+  );
+}
+
+export default Header;
