3267bc497e724f0710799c8ad7bc6cbfb892b859
Marwan Abdelsattar <marwaneg@aucegypt.edu>
2022-09-16 11:00:50 +0200

    collection card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
-
+import { useState, useEffect } from "react";
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://i.seadn.io/gae/9B1oTFEv4DeaW9gFwnd1ION2NzfGnC9lHfjnPNSbNkDZmID0P7Di6Y6ptZxZuf0oaWgnAKLu1eqEbdlCJSpHDFUceo2D7H9c0wK56Q?auto=format&dpr=1.25&w=500"
+      />
     </div>
   );
 }
